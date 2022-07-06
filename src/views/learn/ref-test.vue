0987c6eaa26fd57d7e163f175eb1383d877a5de5
aote777 <1540464777@qq.com>
2022-07-06 08:51:09 +0800

    练习ref

---
+++ src/views/learn/ref-test.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>测试ref</div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  export default defineComponent({
+    name: 'test',
+    data() {
+      return {}
+    },
+  })
+</script>
