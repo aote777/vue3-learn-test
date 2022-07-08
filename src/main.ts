2f30c1082f5a33f759954b4fb1b193ec13e67b3a
aote777 <1540464777@qq.com>
2022-07-08 09:04:51 +0800

    安装axios

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import './service/axios-demo.ts'
+
 // 全局引入
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
