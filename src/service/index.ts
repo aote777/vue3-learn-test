d92879f2016e113d0ace46767ef93d83ca1fa26f
aote777 <1540464777@qq.com>
2022-07-12 14:15:36 +0800

    axios之拦截器的封装

--- src/service/index.ts
+++ src/service/index.ts
@@ -3,4 +3,18 @@ import ccRequest from './request/index'
 export const ccReq = new ccRequest({
   baseURL: process.env.VUE_APP_BASE_URL,
   timeout: 5000,
+  interceptors: {
+    requestInterceptors: (config) => {
+      return config
+    },
+    requestInterceptorsCatch: (err) => {
+      return err
+    },
+    responseInterceptors: (response) => {
+      return response
+    },
+    // responseInterceptorsCatch: (err) => {
+    //   return err
+    // },
+  },
 })
--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,12 +1,34 @@
-import axios from 'axios'
-import type { AxiosInstance, AxiosRequestConfig } from 'axios'
+import axios, { Axios } from 'axios'
+import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
 // axios 可以创建不同的实例
+
+// 定义属于当前封装的类型
+interface ccRequestInterceptors {
+  requestInterceptors?: (config: AxiosRequestConfig) => AxiosRequestConfig
+  requestInterceptorsCatch?: (error: any) => any
+
+  responseInterceptors?: (res: AxiosResponse) => AxiosResponse
+  responseInterceptorsCatch?: (error: any) => any
+}
+
+interface ccRequestConfig extends AxiosRequestConfig {
+  interceptors?: ccRequestInterceptors
+}
 
 class ccRequest {
   instance: AxiosInstance
-
-  constructor(config: AxiosRequestConfig) {
+  interceptors?: ccRequestInterceptors
+  constructor(config: ccRequestConfig) {
     this.instance = axios.create(config)
+    this.interceptors = config.interceptors
+    this.instance.interceptors.request.use(
+      this.interceptors?.requestInterceptors,
+      this.interceptors?.requestInterceptorsCatch
+    )
+    this.instance.interceptors.response.use(
+      this.interceptors?.responseInterceptors,
+      this.interceptors?.responseInterceptorsCatch
+    )
   }
 
   request(config: AxiosRequestConfig): void {
